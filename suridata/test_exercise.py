861b5b66c31d3f61f214ad9bbcfd5c095b578dc8
ridwanray <alabarise@gmail.com>
2023-12-21 19:35:13 +0100

    initial commit

---
+++ suridata/test_exercise.py
@@ -0,0 +1,43 @@
+import unittest
+from exercise import clean_data, chunk_generator, generate_pairs, main
+
+class TestEmployeePairing(unittest.TestCase):
+
+    def test_clean_data(self):
+        employees = [
+            ('John', 'Engineer', 170),
+            ('Doe', 'Doctor', 180),
+            ('John', 'Engineer', 170), # Duplicate
+            ('Smith', 'Artist', 160)
+        ]
+        cleaned_employees = list(clean_data(employees))
+        expected_result = [('John', 'Engineer', 170), ('Doe', 'Doctor', 180), ('Smith', 'Artist', 160)]
+        self.assertEqual(cleaned_employees, expected_result)
+
+    def test_chunk_generator(self):
+        employees = [('John', 'Engineer', 170), ('Doe', 'Doctor', 180), ('Smith', 'Artist', 160)] * 5 # duplicate employees
+        chunks = list(chunk_generator(employees, 2))
+        self.assertEqual(len(chunks), 2)  # 2 chunks 
+
+    def test_generate_pairs(self):
+        """Test the generate_pairs function."""
+        chunked_employees = [('John', 'Engineer', 170), ('Doe', 'Doctor', 180)]
+        pairs = generate_pairs(chunked_employees)
+        self.assertEqual(len(pairs), 1)
+
+    def test_main_function(self):
+        """Test the main function."""
+        employees_list = [
+            ('John', 'Engineer', 170),
+            ('Doe', 'Doctor', 180),
+            ('Smith', 'Artist', 160),
+            ('Jane', 'Lawyer', 175),
+            ('John', 'Engineer', 170),  # Duplicate
+            ('Doe', 'Doctor', 180)  # Duplicate
+        ]
+        unique_pairs = main(employees_list)
+        self.assertIsInstance(unique_pairs, list)
+        self.assertTrue(all(isinstance(pair, tuple) and len(pair) == 2 for pair in unique_pairs))
+
+if __name__ == '__main__':
+    unittest.main()
