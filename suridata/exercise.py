bb2c0ebdfdf4555fefad4df590624ea52f470ec5
ridwanray <34518569+ridwanray@users.noreply.github.com>
2023-12-22 09:01:50 +0100

    update chunk genertor

--- suridata/exercise.py
+++ suridata/exercise.py
@@ -9,7 +9,25 @@ def clean_data(employees: list) -> list:
         key = tuple(each)
         if key not in seen:
             seen.add(key)
-    return seen
+            yield each
+
+
+def chunk_generator(data, chunk_size):
+    """Generate cleaned chunks of data."""
+    chunk = []
+
+    # Loop through cleaned data and yield chunks of specified size
+    for item in clean_data(data):
+        chunk.append(item)
+
+        # Check if the chunk is of the desired size
+        if len(chunk) == chunk_size:
+            yield chunk
+            chunk = []  # empty the chunk container for the next iteration
+
+    # Yield the remaining items if any that do not match the size
+    if chunk:
+        yield chunk
 
 
 def generate_pairs(chunked_employees):
@@ -24,14 +42,15 @@ def generate_pairs(chunked_employees):
         # Ensure that the pair is unique
         if (employee1, employee2) not in pairs and (employee2, employee1) not in pairs:
             pairs.append((employee1, employee2))
+
+    # Handle the case when there is one employee left in the chunk
     return pairs
 
 
 def main(employees):
-    num_chunks = 3 
-    unique_data = list(clean_data(employees))
-    chunk_size = len(clean_data(employees)) // num_chunks
-    chunks = [unique_data[i:i + chunk_size] for i in range(0, len(unique_data), chunk_size)]
+    chunk_size = 4 #chunk size needs to be evenly distibuted (even number)!
+
+    chunks = list(chunk_generator(employees, chunk_size))
 
     # Use multiprocessing Pool to process chunks in parallel
     with Pool() as pool:
