2d2527e972ecfcf9feba3c2199b7b2f597eaaaca
ridwanray <34518569+ridwanray@users.noreply.github.com>
2023-12-22 09:02:47 +0100

    update test cases

--- suridata/test_exercise.py
+++ suridata/test_exercise.py
@@ -1,5 +1,5 @@
 import unittest
-from exercise import clean_data, generate_pairs, main
+from exercise import clean_data, generate_pairs, chunk_generator, main
 
 class TestEmployeePairing(unittest.TestCase):
 
@@ -12,7 +12,12 @@ class TestEmployeePairing(unittest.TestCase):
         ]
         cleaned_employees = list(clean_data(employees))
         self.assertEqual(len(cleaned_employees), 3)
-
+    
+    def test_chunk_generator(self):
+        """Test the chunk_generator function."""
+        employees = [('John', 'Engineer', 170)] * 5  # 5 duplicate employees
+        chunks = list(chunk_generator(employees, 2))
+        self.assertEqual(len(chunks), 1)  
 
     def test_generate_pairs(self):
         """Test the generate_pairs function."""
@@ -27,12 +32,15 @@ class TestEmployeePairing(unittest.TestCase):
             ('Doe', 'Doctor', 180),
             ('Smith', 'Artist', 160),
             ('Jane', 'Lawyer', 175),
+            ('Node', 'Lawyer', 175),
             ('John', 'Engineer', 170),  # Duplicate
             ('Doe', 'Doctor', 180)  # Duplicate
         ]
         unique_pairs = main(employees_list)
         self.assertIsInstance(unique_pairs, list)
         self.assertTrue(all(isinstance(pair, tuple) and len(pair) == 2 for pair in unique_pairs))
+        self.assertEqual(len(unique_pairs), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
